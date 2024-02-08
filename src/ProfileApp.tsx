2253ef0753a42dc9b3b1882d547955b66e59ef84
Low Mei Lin <lmlmeilin@gmail.com>
2024-02-08 16:26:28 +0800

    Update

--- src/ProfileApp.tsx
+++ src/ProfileApp.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import Profile from './components/Profile';
 import ProfileList from './components/ProfileList';
 import NewApp from './NewApp';
+import App from './App';
 
 function ProfileApp() {
   return (
@@ -11,7 +12,7 @@ function ProfileApp() {
       <div>
         {/* Navigation Links */}
         <nav>
-          <Link to="/">Profile</Link> | <Link to="/profiles">Retrieve Profile</Link> | <Link to="/newapp">New App</Link>
+          <Link to="/">Profile</Link> | <Link to="/profiles">Retrieve Profile</Link> | <Link to="/newapp">Admin Create Page</Link> | <Link to="/app">Volunteer Sign Up Page</Link>
         </nav>
 
         {/* Routes */}
@@ -19,6 +20,7 @@ function ProfileApp() {
           <Route path="/" element={<Profile />} />
           <Route path="/profiles" element={<ProfileList />} />
           <Route path="/newapp" element={<NewApp />} />
+          <Route path="/app" element={<App />} />
         </Routes>
       </div>
     </Router>
